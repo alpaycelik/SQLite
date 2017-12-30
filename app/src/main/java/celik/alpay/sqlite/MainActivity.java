750cb12d5aecdd0e16206e0884660aaf02d1ad0e
alpaycelik <alpay.celik@bil.omu.edu.tr>
2017-12-30 10:41:24 +0200

    İlk yükleme

---
+++ app/src/main/java/celik/alpay/sqlite/MainActivity.java
@@ -0,0 +1,13 @@
+package celik.alpay.sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
